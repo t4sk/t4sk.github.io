b73cd5caed002d1e7dffc1d1e87cec6fe99b1182
t4sk <tsk.nakamura@gmail.com>
2019-03-26 21:18:11 +0900

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
+import Image from "react-bootstrap/Image"
+import "./Header.css"
+import Logo from "./Logo.svg"
 
 export function Header(prop) {
   return (
     <Navbar expand="md" fixed>
-      <Navbar.Brand href="/">SMART CONTRACT PROGRAMMER</Navbar.Brand>
+      <Navbar.Brand href="/" className="Header-brand">
+        <div className="Header-brand-img">
+          <Image src={Logo} fluid />
+        </div>
+        SMART CONTRACT PROGRAMMER
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav>
