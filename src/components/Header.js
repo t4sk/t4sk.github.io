23c1bdaa72dfde7882f6b3febe433451a0ef9c38
t4sk <tsk.nakamura@gmail.com>
2019-03-26 14:56:15 +0900

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 
 export function Header(prop) {
-  return <div>Header</div>
+  return (
+    <Navbar expand="md">
+      <Navbar.Brand href="/">SMART CONTRACT PROGRAMMER</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default Header
