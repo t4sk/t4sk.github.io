329f7be571e9a3184515d04e4eef9d6e63b73134
t4sk <tsk.nakamura@gmail.com>
2019-03-29 14:40:55 +0900

    home page

--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export function Home(prop) {
-  return <div>home</div>
-}
-
-export default Home
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../../components/Layout"
+import SEO from "../../components/SEO"
+
+export function Home(prop) {
+  return (
+    <Layout>
+      <SEO title="Smart Contract Programmer" />
+    </Layout>
+  )
+}
+
+export default Home
