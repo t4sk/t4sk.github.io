c1291d6c56c62ccdf64ab0643ff6bfb01515da7f
t4sk <tsk.nakamura@gmail.com>
2019-04-01 14:58:46 +0900

    extract title from components into json

--- scripts/posts-to-json.js
+++ scripts/posts-to-json.js
@@ -1,3 +1,8 @@
+// Transpile all code following this line with babel and use 'env' (aka ES6) preset.
+require("babel-register")({
+  presets: ["env", "react"],
+})
+
 const assert = require("assert")
 const fs = require("fs")
 const path = require("path")
@@ -20,14 +25,17 @@ async function getPosts() {
   return dirs
     .map(dir => {
       const [lang, date] = dir.split("_")
+      const { title } = require(`${POSTS_PATH}/${dir}/index.js`)
 
-      // check valid lang and date
+      // check valid title, lang and date
+      assert(!!title.trim(), `Undefined title ${title}`)
       assert(lang == "en" || lang == "jp", `Invalid lang ${lang} in ${dir}`)
       assert(moment(date).unix() > 0, `Invalid date ${date}`)
 
       return {
         lang,
         date,
+        title,
         // relative path from App.js
         path: `./pages/Blog/posts/${dir}`,
       }
