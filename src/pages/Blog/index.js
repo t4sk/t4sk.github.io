8ee28eafba4ee539bdf1b8e1951440f54f406954
t4sk <tsk.nakamura@gmail.com>
2019-04-03 16:04:31 +0900

    group posts by year

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,29 +1,66 @@
 import React from "react"
+import moment from "moment"
+import PropTypes from "prop-types"
 import Layout from "../../components/Layout"
 import SEO from "../../components/SEO"
 import "./index.css"
 import Posts from "./posts-bk"
 
+export function groupByYear(posts) {
+  const grouped = posts.reduce((grouped, post) => {
+    const year = moment(post.date).year()
+
+    if (grouped[year]) {
+      grouped[year].push(post)
+    } else {
+      grouped[year] = [post]
+    }
+
+    return grouped
+  }, {})
+
+  const years = Object.keys(grouped).sort((a, b) => parseInt(b) - parseInt(a))
+
+  // sort posts by date desc
+  for (let year of years) {
+    grouped[year].sort((a, b) => moment(b.date).unix() - moment(a.date).unix())
+  }
+
+  return {
+    years,
+    byYear: grouped,
+  }
+}
+
 function Base(props) {
   const { lang } = props
 
-  const posts = Posts.filter(post => post.lang == lang)
+  const { years, byYear } = groupByYear(Posts.filter(post => post.lang == lang))
 
   return (
     <Layout lang={lang}>
       <SEO title="Blog | Smart Contract Programmer" lang={lang} />
-      <ul className="Blog-list">
-        {posts.map((post, i) => (
-          <li key={i}>
-            <div className="row">
-              <div className="Blog-date">{post.date}</div>
-              <a href={`/${lang}/blog/${post.date}`}>{post.title}</a>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {years.map((year, i) => (
+        <div key={i}>
+          <h3>{year}</h3>
+          <ul className="Blog-list">
+            {byYear[year].map((post, j) => (
+              <li key={j}>
+                <div className="row">
+                  <div className="Blog-date">{post.date}</div>
+                  <a href={`/${lang}/blog/${post.date}`}>{post.title}</a>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </Layout>
   )
+}
+
+Base.propTypes = {
+  lang: PropTypes.oneOf(["en", "jp"]).isRequired,
 }
 
 const Blog = {
--- src/pages/Blog/index.test.js
+++ src/pages/Blog/index.test.js
@@ -1,9 +1,53 @@
 import React from "react"
 import { shallow } from "enzyme"
-import Blog from "./index"
+import Blog, { groupByYear } from "./index"
 
 test("it renders", () => {
   const component = shallow(<Blog />)
 
   expect(component).toMatchSnapshot()
 })
+
+test("groupByYear", () => {
+  expect(groupByYear(POSTS)).toEqual({
+    years: ["2019", "2018", "2017"],
+    byYear: {
+      "2019": [POSTS[0], POSTS[1]],
+      "2018": [POSTS[2], POSTS[3], POSTS[4]],
+      "2017": [POSTS[5]],
+    },
+  })
+})
+
+const POSTS = [
+  {
+    title: "Introduction to Ethereum Payment Channels Video",
+    date: "2019-01-03",
+    lang: "en",
+  },
+  {
+    title: "Implementing Harberger Tax Deeds",
+    date: "2019-01-02",
+    lang: "en",
+  },
+  {
+    title: "Writing a Dollar Auction Contract",
+    date: "2018-10-11",
+    lang: "en",
+  },
+  {
+    title: "Writing a Penny Auction Contract",
+    date: "2018-10-10",
+    lang: "en",
+  },
+  {
+    title: "Contracts Calling Arbitrary Functions",
+    date: "2018-10-01",
+    lang: "en",
+  },
+  {
+    title: "Testing and Deploying Smart Contracts with Remix",
+    date: "2017-12-12",
+    lang: "en",
+  },
+]
