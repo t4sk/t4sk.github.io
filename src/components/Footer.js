ea4ebb3acb7580c0551b8bc84bf705aee116e80d
t4sk <tsk.nakamura@gmail.com>
2019-04-06 14:42:45 +0900

    footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,20 @@ import Container from "react-bootstrap/Container"
 import GitHubSVG from "./GitHubSVG.svg"
 import "./Footer.css"
 
-// TODO footer
 export function Fotter(prop) {
   return (
     <footer className="Footer">
       <Container>
-        {/*}
-        <a href="https://github.com/t4sk" target="_blank">
-          <img src={GitHubSVG} alt="GitHub" />
-        </a>
-        */}
+        <div className="Footer-row">
+          <a
+            className="Footer-github"
+            href="https://github.com/t4sk"
+            target="_blank"
+          >
+            <img src={GitHubSVG} alt="GitHub" />
+            <span className="Footer-github-text">GitHub</span>
+          </a>
+        </div>
       </Container>
     </footer>
   )
