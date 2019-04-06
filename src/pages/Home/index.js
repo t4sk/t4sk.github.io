b50a68ddfea7f3f5aaf2bfb122b4b6aedefe7f70
t4sk <tsk.nakamura@gmail.com>
2019-04-06 09:55:30 +0900

    fix home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,14 +3,12 @@ import PropTypes from "prop-types"
 import Layout from "../../components/Layout"
 import SEO from "../../components/SEO"
 
-// TODO list most recent posts (both eng and jp)
 function Home(props) {
   const { lang } = props
 
   return (
     <Layout lang={lang}>
       <SEO title="Smart Contract Programmer" lang={lang} />
-      <div>{lang}</div>
     </Layout>
   )
 }
