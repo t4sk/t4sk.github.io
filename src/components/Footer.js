8439fecc0b98299b34221abcd1eb86908da92688
t4sk <tsk.nakamura@gmail.com>
2019-04-22 00:08:06 +0900

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,44 @@
 import React from "react"
 import { Container } from "semantic-ui-react"
-import GitHubSVG from "./GitHubSVG.svg"
+import GitHub from "./GitHub.svg"
 import "./Footer.css"
 
-export function Fotter(prop) {
-  // TODO link to top
+export function Footer(prop) {
   return (
     <footer className="Footer">
       <Container>
         <div className="Footer-row">
-          {/*}
-          <a
-            className="Footer-github"
-            href="https://github.com/t4sk"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={GitHubSVG} alt="GitHub" />
-            <span className="Footer-github-text">GitHub</span>
-          </a>
-          */}
+          <div className="Footer-col" />
+          <div className="Footer-col">
+            <a
+              className="Footer-link"
+              href="https://twitter.com/ProgrammerSmart"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              className="Footer-link"
+              href="https://github.com/t4sk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              className="Footer-link"
+              href="https://www.youtube.com/channel/UCJWh7F3AFyQ_x01VKzr9eyA"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              YouTube
+            </a>
+          </div>
         </div>
       </Container>
     </footer>
   )
 }
 
-export default Fotter
+export default Footer
