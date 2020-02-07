108f3a7d9fb7c2f4d964e00a2b4ffc8bbe348096
t4sk <tsk.nakamura@gmail.com>
2020-02-07 16:42:24 +0900

    show about on front page

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -8,74 +8,75 @@ function About(props) {
 
   return (
     <div className={styles.component}>
-      <Image
-        src={profile}
-        circular
-        fluid
-        style={{ width: 150, height: 150 }}
-        alt="profile"
-      />
-      <h1 className={styles.header}>Tasuku Nakamura</h1>
-      <div className={styles.links}>
+      <div className={styles.profile}>
+        <Image
+          src={profile}
+          circular
+          fluid
+          style={{ width: 150, height: 150 }}
+          alt="profile"
+        />
+        <h1 className={styles.header}>Tasuku Nakamura</h1>
+      </div>
+      <div className={styles.intro}>
+        <p>
+          Hi, I am a software engineer who makes videos about smart contracts.
+        </p>
+        <p>
+          Creator of{" "}
+          <a
+            href="https://solidity-by-example.org/"
+            target="__blank"
+            rel="noopener noreferrer"
+          >
+            Solidity by Example
+          </a>
+        </p>
+      </div>
+
+      <Divider />
+
+      <h3>Follow me</h3>
+      <div>
         <a
-          href="https://www.youtube.com/channel/UCJWh7F3AFyQ_x01VKzr9eyA"
+          href="https://www.youtube.com/channel/UCJWh7F3AFyQ_x01VKzr9eyA/featured"
           target="__blank"
           rel="noopener noreferrer"
         >
-          <Icon name="youtube" color="red" size="huge" />
+          <div className={styles.followMeLink}>
+            <Icon name="youtube" color="red" size="large" />
+            YouTube
+          </div>
         </a>
         <a
           href="https://twitter.com/ProgrammerSmart"
           target="__blank"
           rel="noopener noreferrer"
         >
-          <Icon name="twitter" color="blue" size="huge" />
+          <div className={styles.followMeLink}>
+            <Icon name="twitter" color="blue" size="large" />
+            Twitter
+          </div>
         </a>
         <a
           href="https://github.com/t4sk"
           target="__blank"
           rel="noopener noreferrer"
         >
-          <Icon name="github" color="black" size="huge" />
+          <div className={styles.followMeLink}>
+            <Icon name="github" color="black" size="large" />
+            GitHub
+          </div>
         </a>
       </div>
 
-      <div className={styles.intro}>
-        <p>
-          Hi, I am a software engineer who makes videos about smart contracts.
-        </p>
-        <Divider />
-        <div className={styles.list}>
-          <div className={styles.listItem}>
-            My{" "}
-            <a
-              href="https://www.youtube.com/channel/UCJWh7F3AFyQ_x01VKzr9eyA/featured"
-              target="__blank"
-              rel="noopener noreferrer"
-            >
-              YouTube
-            </a>{" "}
-            channel
-          </div>
-          <div className={styles.listItem}>
-            Creator of{" "}
-            <a
-              href="https://solidity-by-example.org/"
-              target="__blank"
-              rel="noopener noreferrer"
-            >
-              Solidity by Example
-            </a>
-          </div>
-        </div>
-        <Divider />
-        <p>
-          You can contact me at{" "}
-          <span className={styles.email}>
-            contact@smartcontractprogrammer.com
-          </span>
-        </p>
-      </div>
+      <Divider />
+      <p>
+        You can contact me at{" "}
+        <span className={styles.email}>
+          contact@smartcontractprogrammer.com
+        </span>
+      </p>
     </div>
   )
 }
