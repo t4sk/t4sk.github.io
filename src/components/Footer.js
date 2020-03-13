b8e1cdd39318a855844682768a665354bb5f5c14
t4sk <tsk.nakamura@gmail.com>
2020-03-13 16:30:35 +0900

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,27 @@
 import React from "react"
-import { Image } from "semantic-ui-react"
+import { Container, Image } from "semantic-ui-react"
 import styles from "./Footer.module.css"
 import creativeCommonsLogo from "./creative-commons-logo.svg"
 
 export function Footer(props) {
   return (
-    <div className={styles.Footer}>
-      <Image
-        src={creativeCommonsLogo}
-        style={{ height: 20, margin: 10 }}
-        alt="license"
-      />
-      <div>
-        <a
-          href="https://creativecommons.org/licenses/by-sa/3.0/deed.en"
-          target="__blank"
-        >
-          Creative Commons CC BY-SA 3.0
-        </a>
-      </div>
+    <div className={styles.component}>
+      <Container>
+        <div className={styles.row}>
+          <Image
+            src={creativeCommonsLogo}
+            style={{ height: 20, margin: 10 }}
+            alt="license"
+          />
+          <a
+            href="https://creativecommons.org/licenses/by-sa/3.0/deed.en"
+            target="__blank"
+            className={styles.link}
+          >
+            Creative Commons CC BY-SA 3.0
+          </a>
+        </div>
+      </Container>
     </div>
   )
 }
