1ee87edc12454b28f271db93d9c05da2f3be5b5a
t4sk <tsk.nakamura@gmail.com>
2020-04-01 14:44:17 +0900

    tmp disable learn-solidity link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import LearnSolidityPage from "./pages/courses/LearnSolidityPage"
 
 export function App(props) {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
+        {/*}
+        <Route path="/courses/learn-solidity" component={LearnSolidityPage} />
+        */}
         <Route component={HomePage} />
       </Switch>
     </Router>
