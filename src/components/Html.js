ccea37588373a686aeede63940e7ce38d85337e8
t4sk <tsk.nakamura@gmail.com>
2020-04-02 15:16:06 +0900

    html component

---
+++ src/components/Html.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react"
+import PropTypes from "prop-types"
+import hljs from "highlight.js"
+import "highlight.js/styles/atom-one-dark.css"
+import hljsDefineSolidity from "highlightjs-solidity"
+
+function Html(props) {
+  const { html } = props
+
+  useEffect(() => {
+    hljsDefineSolidity(hljs)
+    hljs.initHighlightingOnLoad()
+  }, [])
+
+  return <div dangerouslySetInnerHTML={{ __html: html }}></div>
+}
+
+Html.propTypes = {
+  html: PropTypes.string.isRequired,
+}
+
+export default Html
