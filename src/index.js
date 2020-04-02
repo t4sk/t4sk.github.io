73dc0ba6fdce6830be4f8f7dd519a27841cbfcda
t4sk <tsk.nakamura@gmail.com>
2020-04-02 14:58:03 +0900

    react snap setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react"
-import { render } from "react-dom"
+import { render, hydrate } from "react-dom"
 import "semantic-ui-css/semantic.min.css"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 
 const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
