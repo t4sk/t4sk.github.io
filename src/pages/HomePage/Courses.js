642c79f82435de514674996a047f98ff3a8b3484
t4sk <tsk.nakamura@gmail.com>
2020-04-09 06:14:21 +0900

    fix warnings

--- src/pages/HomePage/Courses.js
+++ src/pages/HomePage/Courses.js
@@ -1,5 +1,5 @@
 import React from "react"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 import styles from "./Courses.module.css"
 import CourseCard from "./CourseCard"
 
--- src/pages/HomePage/EmailSubscriptionForm.js
+++ src/pages/HomePage/EmailSubscriptionForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 import { Form, Input } from "semantic-ui-react"
 import styles from "./EmailSubscriptionForm.module.css"
 
